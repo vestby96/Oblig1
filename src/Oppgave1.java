25fbb5f2a29ddd95a2f975836adfba79364d4491
vestby96 <73116998+vestby96@users.noreply.github.com>
2021-09-09 13:24:50 +0200

    Initial commit

---
+++ src/Oppgave1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Oppgave1 {
+
+    public static void main(String[] args){
+        int[] array = {20,3,4,6,7,3,2,10,15,0,2};
+
+        // Printing the sorted array.
+        System.out.println("Highest value in array : " + maks(array));
+    }
+
+    public static int maks(int[] a)
+    {
+        // Sorterer array slik at det største tallet ender sist
+        for (int i = 0; i < a.length - 1; i++) {
+            int j = i + 1;
+            // Dersom et tall i arrayet er større enn det neste tallet, vil de to tallene bytte plass
+            if (a[i] > a[j]) {
+                // Bytter plass på tallene
+                int value = a[i];
+                a[i] = a[j];
+                a[j] = value;
+            }
+        }
+
+        return a[a.length - 1];
+    }
+}
